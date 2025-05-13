b2a75648b9fe87c5b85777264890f26213284028
estebanrfp <estebanrfp@gmail.com>
2025-05-13 21:50:35 +0200

    HybridClock.js

---
+++ HybridClock.js
@@ -0,0 +1,34 @@
+export class HybridClock {
+  constructor () {
+    this.physical = Date.now()
+    this.logical = 0
+  }
+
+  now () {
+    const currentPhysical = Date.now()
+    this.physical = Math.max(this.physical, currentPhysical)
+    this.logical++
+    return { physical: this.physical, logical: this.logical }
+  }
+
+  update (remoteTimestamp) {
+    if (
+      !remoteTimestamp ||
+      typeof remoteTimestamp.physical !== 'number' ||
+      typeof remoteTimestamp.logical !== 'number'
+    ) return
+
+    this.physical = Math.max(this.physical, remoteTimestamp.physical)
+    this.logical = Math.max(this.logical, remoteTimestamp.logical) + 1
+  }
+
+  compare (ts1, ts2) {
+    if (!ts1 && !ts2) return 0
+    if (!ts1) return -1
+    if (!ts2) return 1
+
+    if (ts1.logical > ts2.logical) return 1
+    if (ts1.logical < ts2.logical) return -1
+    return ts1.physical - ts2.physical
+  }
+}
